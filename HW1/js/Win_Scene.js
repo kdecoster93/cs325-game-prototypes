6622273bc41aa519eb1cc75ce12ce43f68a19a0f
kdecoster93 <47357696+kdecoster93@users.noreply.github.com>
2020-02-19 23:26:15 -0500

    update

--- HW1/js/Win_Scene.js
+++ HW1/js/Win_Scene.js
@@ -6,10 +6,10 @@ class Win_Scene extends Phaser.Scene {
         super ("win_game");
     }
     create() {
-        this.background = this.add.tileSprite(0,0, config.width, config.height, "tiles");
+        this.background = this.add.tileSprite(0,0, 800, 600, "tiles");
         this.background.setOrigin(0,0);
         //this.sky = new Phaser.Display.Color(120, 120, 255);
         //this.cameras.main.setBackgroundColor(sky);
-        this.winText = this.add.text(config.width / 2, config.height / 2, 'You Win!', { font: '96px Roboto', fill: '#ff0000' }); 
+        this.winText = this.add.text(800 / 2, 600 / 2, 'You Win!', { font: '96px Roboto', fill: '#ff0000' }); 
     }
 }
