124360115cc87e5589cbead9f2e58401af96350d
kdecoster93 <47357696+kdecoster93@users.noreply.github.com>
2020-02-19 23:28:07 -0500

    update

--- HW1/js/menu_Scene.js
+++ HW1/js/menu_Scene.js
@@ -11,15 +11,15 @@ class menu_Scene extends Phaser.Scene {
     }
 
     create() {
-        this.menu_background = this.add.image(config.width / 2, config.height / 2, 'menu_background');
+        this.menu_background = this.add.image(800 / 2, 600 / 2, 'menu_background');
         this.menu_background.setScale(0.6);
         //this.background.setOrigin(0,0);
-        this.titleText = this.add.text(175, config.height / 2 - 200, 'LAUNCH!', { font: '96px Roboto', fill: '#ff0000' });
-        this.eText = this.add.text(50, (config.height / 2), 'Press E to Play!', { font: '32px Roboto', fill: '#ffffff' });
-        this.controlsText1 = this.add.text(50, (config.height / 2) + 50, 'Movement: WASD', { font: '32px Roboto', fill: '#ffffff' });
-        this.controlsText2 = this.add.text(50, (config.height / 2) + 100, 'To Fire: Right/Left Click', { font: '32px Roboto', fill: '#ffffff' });
-        this.controlsText3 = this.add.text(50, (config.height / 2) + 150, 'Accelerate: Hold down SHIFT', { font: '32px Roboto', fill: '#ffffff' });
-        this.goalText = this.add.text(50, (config.height / 2) + 200, 'Goal: Hold down shift long enough to reach your \nmaximum velocity and leave the planet!', { font: '32px Roboto', fill: '#ffffff' });
+        this.titleText = this.add.text(175, 600 / 2 - 200, 'LAUNCH!', { font: '96px Roboto', fill: '#ff0000' });
+        this.eText = this.add.text(50, (600 / 2), 'Press E to Play!', { font: '32px Roboto', fill: '#ffffff' });
+        this.controlsText1 = this.add.text(50, (600 / 2) + 50, 'Movement: WASD', { font: '32px Roboto', fill: '#ffffff' });
+        this.controlsText2 = this.add.text(50, (600 / 2) + 100, 'To Fire: Right/Left Click', { font: '32px Roboto', fill: '#ffffff' });
+        this.controlsText3 = this.add.text(50, (600 / 2) + 150, 'Accelerate: Hold down SHIFT', { font: '32px Roboto', fill: '#ffffff' });
+        this.goalText = this.add.text(50, (600 / 2) + 200, 'Goal: Hold down shift long enough to reach your \nmaximum velocity and leave the planet!', { font: '32px Roboto', fill: '#ffffff' });
         // Add the e button to enter the game
         this.e_key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
     }
