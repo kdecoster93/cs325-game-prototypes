d26bf5efd4760b9a5dd839142810e0714a2973b1
kdecoster93 <47357696+kdecoster93@users.noreply.github.com>
2020-02-19 23:53:58 -0500

    update2

--- HW1/js/main.js
+++ HW1/js/main.js
@@ -1,6 +1,6 @@
-//"use strict";
+"use strict";
 
-//window.onload = function() {
+window.onload = function() {
     // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
     // You will need to change the fourth parameter to "new Phaser.Game()" from
     // 'phaser-example' to 'game', which is the id of the HTML element where we
@@ -37,4 +37,4 @@ var config = {
 	}
 
 	var game = new Phaser.Game(config);
-//}
+}
